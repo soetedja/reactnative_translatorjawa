1038380491caf8b18caf9c1b5a42d5fbd76fce6f
soetedja <soetedja@gmail.com>
2022-06-15 22:20:49 +0700

    Migrate

--- src/config/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  google: {
-    client_id:
-      '882768718362-0p3s45hitrefjbf9usaaucqp6qab3kru.apps.googleusercontent.com',
-    maps_api_key: 'AIzaSyAkONXnwJ4SxMqOqq8XVqWnIOlUb2PxV-8',
-  },
-};
--- src/config/keys.js
+++
@@ -1,9 +0,0 @@
-const keysDev = require('./keys.dev');
-const keysProd = require('./keys.prod');
-
-// eslint-disable-next-line no-undef
-if (__DEV__) {
-  module.exports = keysDev;
-} else {
-  module.exports = keysProd;
-}
